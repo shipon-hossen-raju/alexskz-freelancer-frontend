f120b40662ff6c16c834359d56bf9aba449b30ba
Shipon Hossen Raju <msshipon234@gmail.com>
2026-01-01 14:00:02 +0600

    build issue solved

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	// Increase Server Actions body size limit to allow larger uploads (default is 1 MB)
-	// Set as a number (bytes). Here we set the limit to 20 MB.
-	serverActions: {
-		bodySizeLimit: 20 * 1024 * 1024, // 20 MB
-	},
-
-	// Allow Cloudinary images to be used with next/image
+  // Increase Server Actions body size limit to allow larger uploads (default is 1 MB)
+  // Set as a number (bytes). Here we set the limit to 20 MB.
+//   serverActions: {
+//     bodySizeLimit: 20 * 1024 * 1024, // 20 MB
+//   },
+//   reactStrictMode: true,
+  // Allow Cloudinary images to be used with next/image
   images: {
-    domains: ['res.cloudinary.com'], // Allow Cloudinary domain
+    domains: ["res.cloudinary.com"], // Allow Cloudinary domain
   },
 };
 
